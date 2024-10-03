8fd05a7fd4b26256fa7d63920cc523525c6492b4
erick <erickwelberth1@hotmail.com>
2024-10-02 21:44:27 -0300

    feat: product description correction feature

--- src/main/java/martapi_rest/mart/food/FoodResponseDTO.java
+++ src/main/java/martapi_rest/mart/food/FoodResponseDTO.java
@@ -1,7 +1,7 @@
 package martapi_rest.mart.food;
 
-public record FoodResponseDTO(Long id, String title, String image, Integer price) {
+public record FoodResponseDTO(Long id, String title, String description, String image, Integer price) {
     public FoodResponseDTO(Food food) {
-        this(food.getId(), food.getTitle(), food.getImage(), food.getPrice());
+        this(food.getId(), food.getTitle(), food.getDescription(), food.getImage(), food.getPrice());
     }
 }
